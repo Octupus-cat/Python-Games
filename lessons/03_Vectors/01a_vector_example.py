920176cb6a4959020f8dc0ff5aa9fd27743a0f3c
Octupus-cat <Catherine@garibaldibros.com>
2025-01-18 20:58:46 +0000

    i finnished 01a_vector_example today

--- lessons/03_Vectors/01a_vector_example.py
+++ lessons/03_Vectors/01a_vector_example.py
@@ -18,13 +18,15 @@ Vector20, draw_v20, draw_grid = Vector20Factory(screen_width, screen_height, 20)
 draw_grid(screen)
 
 # Create some vectors
-v0 = Vector20(0,0)
-v1 = Vector20(8, 8)  
+v0 = Vector20(0, 0)
+v1 = Vector20(0, 1) * 10 
 v2 = Vector20(3, -12)  
 v3 = Vector20(-4, -2)  
 v4 = Vector20(-12, 0) 
 v5 = Vector20(0, 12)
-
+v2.rotate_ip(104)
+v3.rotate_ip(296)
+v4.rotate_ip(180)
 start = draw_v20(screen, v0, v1)
 start = draw_v20(screen, start, v2)
 start = draw_v20(screen, start, v3)
